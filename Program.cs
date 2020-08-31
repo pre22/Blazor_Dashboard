a8bf2fec6d96bc01eaa67fc5be7ca125d92b7abe
Pre22 <pre.chika.22@gmail.com>
2020-08-31 11:08:25 +0100

    added gitignore
    I configured the blazor package and added gitignore

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor; 
 
 namespace Blazor_Dashboard
 {
@@ -14,8 +15,10 @@ namespace Blazor_Dashboard
     {
         public static async Task Main(string[] args)
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzEwODQ2QDMxMzgyZTMyMmUzMEo1b2xDMklRekN0UkRkdlBJb0hTMmNzVlBSamc2QVFYYjhhdWhFYVB1eGs9");
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Services.AddSyncfusionBlazor();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
